1da297ca725b2286769bce84ec7c7d70f88f723f
khurramjaved96 <14besekjaved@seecs.edu.pk>
2016-11-26 13:07:47 +0500

    Cleaning code

---
+++ testing.py
@@ -0,0 +1,7 @@
+import numpy as np
+import tensorflow as tf
+import cv2
+
+import utils
+
+utils.compute_features("./Challenge2_Training_Task12_Images/101.jpg")
--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import skimage
 import skimage.io
 import skimage.transform
 import numpy as np
+import vgg16
+import tensorflow as tf
 
 
 # synset = [l.strip() for l in open('synset.txt').readlines()]
@@ -14,59 +16,34 @@ def load_image(path):
     img = skimage.io.imread(path)
     img = img / 255.0
     assert (0 <= img).all() and (img <= 1.0).all()
-    # print "Original Image Shape: ", img.shape
-    # we crop image from center
-    short_edge = min(img.shape[:2])
-    yy = int((img.shape[0] - short_edge) / 2)
-    xx = int((img.shape[1] - short_edge) / 2)
-    crop_img = img[yy: yy + short_edge, xx: xx + short_edge]
-    # resize to 224, 224
-    resized_img = skimage.transform.resize(crop_img, (1000, 1000))
     return img
 
 
-# returns the top1 string
-def print_prob(prob, file_path):
-    synset = [l.strip() for l in open(file_path).readlines()]
+def compute_features(path, network = "VGG16", extension=".jpg"):
+    
+    img1 = load_image(path) 
+    print "Shape = " , img1.shape
+    if(network == "VGG16"):
+        batch = np.expand_dims(img1,axis=0)
 
-    # print prob
-    pred = np.argsort(prob)[::-1]
+        with tf.Session(
+                config=tf.ConfigProto(gpu_options=(tf.GPUOptions(per_process_gpu_memory_fraction=0.7)))) as sess:
+            images = tf.placeholder("float", [1, img1.shape[0], img1.shape[1], img1.shape[2]])
+            feed_dict = {images: batch}
 
-    # Get top1 label
-    top1 = synset[pred[0]]
-    print("Top1: ", top1, prob[pred[0]])
-    # Get top5 label
-    top5 = [(synset[pred[i]], prob[pred[i]]) for i in range(5)]
-    print("Top5: ", top5)
-    return top1
+            vgg = vgg16.Vgg16()
+            with tf.name_scope("content_vgg"):
+                vgg.build(images)
 
-
-def load_image2(path, height=None, width=None):
-    # load image
-    img = skimage.io.imread(path)
-    img = img / 255.0
-    if height is not None and width is not None:
-        ny = height
-        nx = width
-    elif height is not None:
-        ny = height
-        nx = img.shape[1] * ny / img.shape[0]
-    elif width is not None:
-        nx = width
-        ny = img.shape[0] * nx / img.shape[1]
+            features = sess.run(vgg.conv5_3, feed_dict=feed_dict)
+            print features[0].shape
+            np.save(path, features[0])
     else:
-        ny = img.shape[0]
-        nx = img.shape[1]
-    return skimage.transform.resize(img, (ny, nx))
-
+        print ("Implement functionality of your desired model")
 
 def test():
-    img = skimage.io.imread("./test_data/starry_night.jpg")
-    ny = 300
-    nx = img.shape[1] * ny / img.shape[0]
-    img = skimage.transform.resize(img, (ny, nx))
-    skimage.io.imsave("./test_data/test/output.jpg", img)
-
+    print "Testing"
+    compute_features("./Challenge2_Training_Task12_Images/101.jpg")
 
 if __name__ == "__main__":
     test()
