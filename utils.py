2566ebb37ae2d268a7c902ebdfcb17863e7104ca
khurramjaved96 <14besekjaved@seecs.edu.pk>
2016-11-26 16:08:17 +0500

    Adding Image Class

--- utils.py
+++ utils.py
@@ -11,19 +11,39 @@ import tensorflow as tf
 
 # returns image of shape [224, 224, 3]
 # [height, width, depth]
-def load_image(path):
-    # load image
-    img = skimage.io.imread(path)
-    img = img / 255.0
-    assert (0 <= img).all() and (img <= 1.0).all()
-    print img.shape
-    if(img.shape[0] < img.shape[1]):
-        image = skimage.transform.resize(img, (600, int(600.0/img.shape[0]*float(img.shape[1])),3))
-    else:
-        image = skimage.transform.resize(img, (int(600.0/img.shape[1]*img.shape[0]),600,3))
-    batch = np.expand_dims(image,axis=0)
-    print batch.shape
-    return batch
+
+class image_data: 
+    def __init__(self, path):
+
+        self.orignal_image = self.load_image(path)
+        self.image=self.resize_image(self.orignal_image)
+        self.data = self.expand_dims(self.image)
+        self.scale = float(self.image.shape[0])/float(self.orignal_image.shape[0])
+
+        print "Oringal Image " , self.orignal_  bimage.shape
+        print "Resized Image" , self.image.shape
+
+        print "Scale = " , self.scale
+
+    def resize_image(self, img):
+        if(img.shape[0] < img.shape[1]):
+            image = skimage.transform.resize(img, (600, int(600.0/img.shape[0]*float(img.shape[1])),3))
+        else:
+            image = skimage.transform.resize(img, (int(600.0/img.shape[1]*img.shape[0]),600,3))
+        return image
+
+    def expand_dims(self,img):
+        return np.expand_dims(img,axis=0)
+
+
+    def load_image(self,path):
+        # load image
+        img = skimage.io.imread(path)
+        img = img / 255.0
+        assert (0 <= img).all() and (img <= 1.0).all()
+
+        return img
+
 
 
 def compute_features(path, network = "VGG16", extension = "jpg"):
@@ -67,13 +87,14 @@ def compute_features(path, network = "VGG16", extension = "jpg"):
             for file in os.listdir(path):
                 if file.endswith(extension):
                     print(file)
-                    img =  load_image(path+"/"+file)
+                    img = image_data(path+"/"+file).data
+                    #img =  load_image(path+"/"+file)
                     print img.shape
                     feed_dict = {images: img}
 
                     features = sess.run(vgg.conv5_3, feed_dict=feed_dict)
                     print features[0].shape
-                    np.save(path+"/"+file, features[0])
+                    #np.save(path+"/"+file, features[0])
 
     else:
         print ("Implement functionality of your desired model")
@@ -81,7 +102,7 @@ def compute_features(path, network = "VGG16", extension = "jpg"):
 def test():
     print "Testing"
 
-    compute_features("./Challenge2_Training_Task12_Images")
+    compute_features("./Train_Images")
 
 if __name__ == "__main__":
     test()
